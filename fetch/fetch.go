38d9868e57ba213afc9cb95fb3e5ccf69720dd34
Mamf <contact@mschrauth.de>
2023-08-05 11:22:30 +0200

    fetch reviews of certain critic

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -3,13 +3,22 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 )
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
 
 type Critic struct {
 	name string
@@ -80,13 +89,161 @@ func fetch_critics(outFile string) {
 		}
 	}
 	fmt.Println("\r Writing to file: 100%")
+}
 
+type Review struct {
+	score      string
+	mediaTitle string
+	mediaInfo  string
+	mediaUrl   string
+}
+
+func (r Review) String() string {
+	return fmt.Sprintf("%s;%s;%s;%s",
+		strings.ReplaceAll(r.score, ";", "\\;"),
+		strings.ReplaceAll(r.mediaTitle, ";", "\\;"),
+		strings.ReplaceAll(r.mediaInfo, ";", "\\;"),
+		strings.ReplaceAll(r.mediaUrl, ";", "\\;"))
+}
+
+type ReviewBatch struct {
+	reviews []Review
+	next    string
+	prev    string
+}
+
+type pageInfo struct {
+	HasNextPage     bool
+	HasPreviousPage bool
+	StartCursor     string
+	EndCursor       string
+}
+
+type rawReview struct {
+	OriginalScore string
+	MediaInfo     string
+	MediaTitle    string
+	MediaUrl      string
+}
+
+type rawResp struct {
+	PageInfo pageInfo
+	Reviews  []rawReview
+}
+
+func parseReviewBatch(json_raw []byte) ReviewBatch {
+	res := rawResp{}
+	json.Unmarshal(json_raw, &res)
+
+	var reviews []Review
+
+	for _, rev := range res.Reviews {
+		reviews = append(reviews, Review{
+			score:      rev.OriginalScore,
+			mediaTitle: rev.MediaTitle,
+			mediaInfo:  rev.MediaInfo,
+			mediaUrl:   rev.MediaUrl,
+		})
+	}
+
+	batch := ReviewBatch{
+		reviews: reviews,
+		next:    res.PageInfo.EndCursor,
+		prev:    res.PageInfo.StartCursor,
+	}
+
+	return batch
+}
+
+func getFirstBatch(critic *Critic) (*ReviewBatch, error) {
+	fmt.Println("Get first batch...")
+
+	regexp, err := regexp.Compile("<script id=\"reviews-json\" .+>(.+)</script>")
+	if err != nil {
+		return nil, err
+	}
+
+	const url = "https://www.rottentomatoes.com/critics/%s/movies"
+
+	resp, err := http.Get(fmt.Sprintf(url, critic.url))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	raw_body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	body := string(raw_body)
+
+	match := regexp.FindStringSubmatch(body)
+
+	json_raw := []byte(match[1])
+
+	batch := parseReviewBatch(json_raw)
+
+	return &batch, nil
+}
+
+func getBatch(critic *Critic, afterCursor string) (*ReviewBatch, error) {
+	const url = "https://www.rottentomatoes.com/napi/critics/%s/movies?after=%s&pagecount=50"
+
+	resp, err := http.Get(fmt.Sprintf(url, critic.url, afterCursor))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	raw_body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	batch := parseReviewBatch(raw_body)
+	return &batch, nil
+}
+
+func fetch_reviews(critic *Critic, verbose bool) ([]Review, error) {
+	var reviews []Review
+	batch, err := getFirstBatch(critic)
+	if err != nil {
+		return nil, err
+	}
+
+	reviews = batch.reviews
+	var page_count = 2
+
+	var next = batch.next
+	for len(next) > 0 {
+		if verbose {
+			fmt.Printf("\rLoad Review page %d...", page_count)
+		}
+		page_count += 1
+
+		batch, err = getBatch(critic, next)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		reviews = append(reviews, batch.reviews...)
+		next = batch.next
+	}
+	if verbose {
+		fmt.Println()
+	}
+
+	return reviews, nil
 }
 
 func main() {
 	fetchCriticsSet := flag.NewFlagSet("fetch-critics", flag.ExitOnError)
-
 	var outFile = fetchCriticsSet.String("o", "./tmp/out.txt", "Path to the out-file")
+
+	fetchReviewsSet := flag.NewFlagSet("fetch-reviews", flag.ExitOnError)
+	var criticUrl = fetchReviewsSet.String("c", "", "URL of critic to get reviews from")
 
 	if len(os.Args) < 2 {
 		fmt.Println("Expect arguments")
@@ -97,6 +254,17 @@ func main() {
 	case "fetch-critics":
 		fetchCriticsSet.Parse(os.Args[2:])
 		fetch_critics(*outFile)
+	case "fetch-reviews":
+		fetchReviewsSet.Parse(os.Args[2:])
+		reviews, err := fetch_reviews(&Critic{name: "", url: *criticUrl}, true)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Found %d reviews\n", len(reviews))
+		for _, rev := range reviews[:min(len(reviews), 10)] {
+			fmt.Println(rev.String())
+		}
 	default:
 		fmt.Printf("Unkown command \"%s\"\n", os.Args[1])
 		os.Exit(1)
